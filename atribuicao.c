674fb43a93b547387be80845a2664c2b1349f866
SamuelViana-work <samuelviana.work@gmail.com>
2024-05-16 22:20:30 -0300

    primeiro exercicio

---
+++ atribuicao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int valor; //criei uma variavel para guardar valor do tipo inteiro
+valor = 666; //atribuí um valor para a variavel valor
+
+
+int main (){
+printf("Valor da minha variavel:%d", valor);
+
+return 0;
+
+}
