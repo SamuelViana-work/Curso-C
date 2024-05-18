2c09e3169193959c69cadc55c25ae751c5f96f73
SamuelViana-work <samuelviana.work@gmail.com>
2024-05-18 15:52:22 -0300

    primeiro commit

--- atribuicao.c
+++ atribuicao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int valor; //criei uma variavel para guardar valor do tipo inteiro
+/*int valor; //criei uma variavel para guardar valor do tipo inteiro
 valor = 666; //atribuí um valor para a variavel valor
 
 
@@ -13,4 +13,20 @@ printf("Valor da minha variavel:%d", valor);
 
 return 0;
 
+}
+*/
+
+float numero = 3.1415;
+
+
+
+int main() {
+
+   // printf("o valor da minha variavel eh %.4f", numero);
+
+    printf("Digite um numero real: ");
+    scanf("%f", &numero);
+    printf("O valor digitado foi %.2f", numero);
+
+return 0;
 }
