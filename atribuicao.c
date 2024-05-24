368866a054caa0501fe781da84a9e47a29dc0f12
SamuelViana-work <samuelviana.work@gmail.com>
2024-05-23 21:12:22 -0300

    att exercicios 2.0

--- atribuicao.c
+++ atribuicao.c
@@ -1,16 +1,228 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> 
+//setlocale(LC_ALL, "English");
 
+/*
+            ---ATRIBUINDDO VALOR A UMA VARIAVEL DO TIPO INT---
 int valor; //criei uma variavel para guardar valor do tipo inteiro
 valor = 666; //atribuí um valor para a variavel valor
 
-
 int main (){
-printf("Atribua um valor para a sua variavel:");
-scanf("%d", &valor);
-
-printf("Valor da minha variavel:%d", valor);
+    
+    printf("Atribua um valor para a sua variavel:");
+    scanf("%d", &valor);
+    printf("Valor da minha variavel:%d", valor);
 
 return 0;
 
 }
+
+            ---ATRIBUINDO VALOR A UMA VARIAVEL DO TIPO FLOAT---
+float numero = 3.1415;
+
+int main() {
+
+//  printf("o valor da minha variavel eh %.4f", numero);
+
+    printf("Digite um numero real: ");
+    scanf("%f", &numero);
+    printf("O valor digitado foi %.2f", numero);
+  
+return 0;
+
+}
+
+            ---ATRIBUINDO VALOR A UMA VARIAVEL DO TIPO CHAR---
+char sexo = 's';
+
+int main() {
+    printf("Digite seu sexo: (f, F, m ou M)");
+    scanf("%c", &sexo);
+    printf("valor da variavel sexo:%c", sexo);
+
+return 0;
+}
+
+            ---UTILIZANDO GETCHAR PARA LER VARIAVEL DO TIPO CHAR---
+char letra;
+
+int main (){
+    printf("Digite uma letra:");
+    letra = getchar();
+    
+    printf("Valor da variavel letra:%c",letra); 
+
+return 0;
+}
+
+            ---UTILIZANDO A FUNÇÃO GETC PARA LER VARIAVEL DO TIPO CHAR---
+char letra;
+
+int main (){
+    printf("Digite um caracter:");
+    letra = getc(stdin);
+    
+    printf("Caracter lido:%c",letra); 
+
+return 0;
+}
+
+            ---UTILIZANDO GETC PARA LER VARIAVEL DO TIPO CHAR---
+char letra;
+
+int main (){
+    printf("Digite um caracter:");
+    letra = fgetc(stdin);
+    
+    printf("Caracter lido:%c",letra); 
+
+return 0;
+}
+        ---LENDO VÁRIOS VALORES---
+int n1, n2, n3;
+
+int main(){
+    printf("Escreva 3 numeros inteiros: ");
+    scanf("%d %d %d",&n1, &n2, &n3);
+    printf("Os valores digitados foram: %d, %d e %d", n1, n2, n3);
+
+return 0;
+}
+
+            //CRIANDO UM PROGRAMA PRA LER NOME, IDADE, PESO E ALTURA
+char sexo;
+int idade;
+float peso, altura;
+
+int main(){
+    printf("DIGITE SEXO(F OU M), IDADE, PESO E ALTURA!\n");
+    scanf("%c %d %f %f ", &sexo, &idade, &peso, &altura);
+    printf("SEXO: %c\n IDADE: %d\n PESO: %.3f\n ALTURA: %2.f\n", sexo, idade, peso, altura);
+
+
+}
+return 0;
+
+            // EXIBINDO O TAMANHO DE UMA VARIAVEL NA MAEMÓRIA
+
+int main(){
+    printf("O tamanho de um float na memoria eh: %d bytes\n", sizeof(float));
+
+    return 0;
+}
+
+            // INCREMENTANDO 1 A UM VALOR 
+
+short int x = 664;
+
+int main(){
+    x++;
+    printf("Valor de x = %d\n", x);
+
+return 0;
+}
+
+long long int x = 100000000;
+
+int main(){
+    printf("Tamano de x:  ")
+}
+
+float x = 3.1415;
+
+int main(){
+    printf("UMA VARIAVEL DO TIPO FLOAT OCUPA:%.4f\n",x );
+
+}
+
+long double x = 6.6669;
+
+int main(){
+    printf("Uma variavel long float ocupa %d bytes na memoria\n", sizeof(x));
+    __mingw_printf("a variavel x tem o valor de: %.4Lf\n", x);
+
+return 0;
+}
+
+            //---CARACTERE DE ESCAPE---
+int main(){
+    printf("\n ESPACAMENTO\n");
+    printf("\t TABULACAO\n");
+    printf(" \"\ \'\ \n");
+    printf("\\\n");
+
+return 0;
+
+}
+            //---OPERACOES MATEMATICAS E CONVERSAO DE TITULO---
+int a = 666, b = 69;
+
+int main(){
+    printf("RESULTADO DA SOMA \'A'\ + \'B'\:%d\n", a+b);
+    printf("RESULTADO DA SUBTRACAO \'A'\ - \'B'\:%d\n", a-b);
+    printf("RESULTADO DA MULTIPLICACAO \'A'\ * \'B'\:%d\n", a*b);
+    printf("RESULTADO DA DIVISAO \'A'\ / \'B'\:%d\n", a/(float)b);
+    
+return 0;
+}
+
+            //---OPERADOR DE RESTO DA DIVISAO---
+int a = 10, b = 7;
+
+int main(){
+    printf("Resto da divisao inteira %d\n", a % b);
+
+
+return 0;
+}
+
+            //---OPERADOR DE INCREMENTO---
+
+
+int contador = 69;
+
+int main(){
+    printf("Valor: %d\n", ++contador);
+    //para decremento é a mesma coisa só que com o --
+
+
+return 0;
+}
+
+            //---UTILIZANDO ACENTUAÇÃO---
+    //setscale(LC_ALL, NULL)->PADRAO DA LINGUAGEM C
+    //setscale(LC_ALL, "")->PADRAO DA LINGUAGEM DO SISTEMA OPERACIONAL
+    //setscla(LC_ALL, "Portuguese")->PADRAO DA LINGUA DEFINIDA ENTRE ""(aconcelhavel que seja a mesma do SO)
+ int main(){
+    printf("%s\n", setlocale(LC_ALL,NULL));
+
+    printf("BATATÃO");
+
+return 0;
+ }
+ 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercicios.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> 
+
+
+/*Curso de Programação C – Professor Wagner Gaspar
+Exercícios: lista 1
+
+5) Uma empresa contrata um encanador a R$ 45,00 por dia. Faça um programa que solicite o
+número de dias trabalhados pelo encanador e imprima a quantia líquida que deverá ser paga,
+sabendo que são descontados 8% para imposto de renda.
+
+6) Crie um programa em C que permita fazer a conversão cambial entre Reais e Dólares. Considere
+como taxa de câmbio US$1,0 = R$5,30. Leia um valor em Reais e mostre o correspondente em
+Dólares.
+
+7) Faça um programa para ler do teclado uma quantidade de segundos e imprimir na tela a
+conversão para horas, minutos e segundos.
+Exemplo:
+Entrada: 3672
+Saída: 1:1:12
+
+*/
+
+            //---QUESTÃO 2---
+/*2) Elabore um algoritmo que receba, por meio do teclado, dois valores, um para a variável “a” e um
+para a variável “b”. Em seguida, faça os passos que julgar necessário para que ao final, a variável
+“a” possua o valor que inicialmente estava em “b” e a variável “b” possua o valor que inicialmente
+estava em “a”. Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+
+
+int a , b;
+
+int main(){
+    printf("\n\t Insira o valor de \'A'");
+    scanf("\n%d", &a);
+    printf("\n\t\'A'vale:%d\n", a);
+    
+    printf("\n\t Insira o valor de \'B'");
+    scanf("\n%d", &b);
+    printf("\n\t\'B'vale:%d\n", b);
+
+    setlocale(LC_ALL,"Portuguese");
+    printf("\n\t RESPOSTA QUESTÃO 2:\n");
+    printf("\n\t'A'vale:%d\n", b);
+    printf("\n\t'B'vale:%d\n", a);
+
+return 0;
+
+}
+*/
+
+            /*---QUESTÃO 3---
+3) Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
+variável auxiliar.
+
+int x, y;
+
+int main(){
+    printf("\n\t DEFINA O VALOR DE 'X' e 'Y':\n");
+    scanf("%d%d",&y, &x);
+    setlocale(LC_ALL,"Portuguese");
+    printf("\n\t RESPOSTA DA QUESTÃO 3:\n X=%d Y=%d\n", x, y);
+
+return 0;
+}
+*/
+
+            /*--QUESTÃO 4---
+4) Escreva um programa que leia um valor de despesa de restaurante, o valor da gorjeta (em
+porcentagem) e o número de pessoas para dividir a conta. Imprima o valor que cada um deve pagar.
+Assuma que a conta será dividida igualmente.
+*/
+
+float conta, gorjeta, despesa;
+int pagantes;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    printf("\n\t BEM VINDO AO RESTAURANTE 'PAGA CARO'\n");
+    
+    printf("\n\t Insira o valor da conta:");
+    scanf("%f", &conta);
+    printf("\n\t Seu conta custa: R$ %.2f\n", conta);
+
+    printf("\n\t Insira o valor da gorjeta:");
+    scanf("%f", &gorjeta);
+    printf("\n\t Voce deu: R$ %.2f de gorjeta\n", gorjeta);
+
+    printf("\n\t Insira quantas pessoas irão pagar a conta:");
+    scanf("%d", &pagantes);
+    printf("\n\t O valor de R$ %.2f será dividido igualmente entre os pagantes!",conta+gorjeta);
+
+    printf("\n\t O valor que deverá ser pago por cada pagante é: R$ %.2f",(conta+gorjeta)/pagantes);
+
+return 0;
+}
